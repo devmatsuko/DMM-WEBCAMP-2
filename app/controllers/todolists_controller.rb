58135764407302df1b6dc1b2cbaba2e1817cfef4
EC2 Default User <ec2-user@ip-172-31-76-156.ec2.internal>
2020-09-28 08:31:16 +0000

    [Add] 開発スキルアップ用のgemファイルの追加

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -16,22 +16,23 @@ class TodolistsController < ApplicationController
 
   def index
     @lists = List.all
+    puts "作成したキー #{ENV['SECRET_KEY']}"
   end
 
   def show
     @list = List.find(params[:id])
   end
-  
+
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to todolist_path(list.id)
   end
-  
+
   def destroy
     list = List.find(params[:id])  # データ（レコード）を1件取得
     list.destroy  # データ（レコード）を削除
